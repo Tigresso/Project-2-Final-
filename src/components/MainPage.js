0cb68d4ca91efa8e5d54b36f5bd23ac1a570ed7c
Tigresso <kayla10463@yahoo.com>
2022-05-22 18:07:23 -0400

    blank start

---
+++ src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MainPage extends React.Component{
+    render(){
+        return(
+            <div>
+                Start Layout
+            </div>
+        );
+    }
+}
+
+export default MainPage;
